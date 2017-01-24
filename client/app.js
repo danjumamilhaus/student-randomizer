4a880c8bda8341720c5c60ec0f73e04077e7e063
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-24 17:54:22 -0600

    Implement controller and factory

--- client/app.js
+++ client/app.js
@@ -1,1 +1,22 @@
+angular.module('randomizer', []);
+
 angular.module('randomizer')
+  .controller('rando-ctrl', ['$scope', 'randoFactory',function($scope, randoFactory) {
+    $scope.student = randoFactory.counter;
+    console.log(randoFactory.counter);
+
+    $scope.getRandom = function() {
+      randoFactory.counter++
+      $scope.student = randoFactory.counter;
+    };
+  }])
+  .factory('randoFactory', function($http) {
+    var counter = 0;
+
+    return {
+      counter: counter
+    };
+  });
+
+
+
