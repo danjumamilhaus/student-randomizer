2bc7e71337f286adb5b4ca2c822f7f206eafe598
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-24 12:08:12 -0600

    Test mongo

--- server/server-config.js
+++ server/server-config.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/students' || 'mongodb://heroku_899x2dvs:nm3eu0klm66elkf99oinqacgbh@ds127389.mlab.com:27389/heroku_899x2dvs');
+//mongoose.connect('mongodb://localhost/students' || 'mongodb://heroku_899x2dvs:nm3eu0klm66elkf99oinqacgbh@ds127389.mlab.com:27389/heroku_899x2dvs');
 
 app.get('/', function(req, res) {
   res.send('bacon');
