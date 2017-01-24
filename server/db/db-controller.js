48f4d71cede55625f86246f6655f2c29ae71304c
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-24 12:53:29 -0600

    Implement seed and get student function

--- server/db/db-controller.js
+++ server/db/db-controller.js
@@ -1,13 +1,26 @@
 var Student = require('./db-model.js');
+var seed = require('./seed.js');
 
-module.exports = {
-  allStudents: function(req,res, next) {
-    Student.find()
-    .then(function(users) {
-      res.send(users);
-    })
-    .catch(function(err) {
-      console.error(err);
+Student.collection.insertMany(seed);
+
+exports.getRandomStudent = function(req, res, next) {
+  Student.find()
+    .limit(-1)
+    .skip(Math.floor(Math.random() * 30) + 1);
+    .next(function(user) {
+      console.log(user);
+      res.send(user);
     });
-  }
 };
+
+// module.exports = {
+//   allStudents: function(req,res, next) {
+//     Student.find()
+//     .then(function(users) {
+//       res.send(users);
+//     })
+//     .catch(function(err) {
+//       console.error(err);
+//     });
+//   }
+// };
--- server/server-config.js
+++ server/server-config.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var StudentCtrl = require('./db/db-controller.js')
 
 var app = express();
 
 mongoose.connect('mongodb://heroku_899x2dvs:nm3eu0klm66elkf99oinqacgbh@ds127389.mlab.com:27389/heroku_899x2dvs');
 
 app.get('/', function(req, res) {
-  res.send('bacon');
+  // res.send('bacon');
+  res.send(StudentCtrl.getRandomStudent());
 });
 
 module.exports = app;
