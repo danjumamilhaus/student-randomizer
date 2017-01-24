6e30923f8cb68c56134536089b9f63ac23d08952
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-23 22:16:50 -0600

    Add angular-routes, add mdb process env

--- server/server-config.js
+++ server/server-config.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/stud-rando');
+mongoose.connect('mongodb://localhost/students' || process.env.MONGODB_URI);
 
 app.get('/', function(req, res) {
   res.send('bacon');
