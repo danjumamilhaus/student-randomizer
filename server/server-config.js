9864943745cf166eb8cd80553400869d476a4ccc
Daniel Ritchie <danjumamilhaus@gmail.com>
2017-01-24 11:55:59 -0600

    Add mongo uri

--- server/server-config.js
+++ server/server-config.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 
 var app = express();
 
-mongoose.connect('mongodb://localhost/students' || process.env.MONGODB_URI);
+mongoose.connect('mongodb://localhost/students' || 'mongodb://danjumamilhaus@gmail.com:studentrandomizer@ds127389.mlab.com:27389/heroku_899x2dvs');
 
 app.get('/', function(req, res) {
   res.send('bacon');
